2d1886bca6ff1b67f6eb4ca90317a38014853c44
Marvin Vogt <m@rvinvogt.com>
2024-08-20 20:36:17 +0000

    Add ignition user data

--- main.tf
+++ main.tf
@@ -21,6 +21,8 @@ resource "openstack_compute_instance_v2" "instance" {
     uuid = openstack_networking_network_v2.network.id
   }
   security_groups = ["default", "allow-all"]
+
+  user_data = file("ignition.json")
 }
 resource "openstack_compute_volume_attach_v2" "attached" {
   instance_id = openstack_compute_instance_v2.instance.id
