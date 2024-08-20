196da323a707a772b7864ca7cd97e56054fd1c0c
Marvin Vogt <m@rvinvogt.com>
2024-08-20 20:16:18 +0000

    Add instance with attached volume

--- main.tf
+++ main.tf
@@ -8,3 +8,21 @@ resource "openstack_blockstorage_volume_v3" "data" {
   name = "banzai"
   size = 1000
 }
+
+resource "openstack_compute_instance_v2" "instance" {
+  name                = "banzai"
+  image_name          = "Flatcar_Production - Latest"
+  flavor_name         = "s1.medium"
+  key_pair            = openstack_compute_keypair_v2.generic.name
+  config_drive        = true
+  stop_before_destroy = true
+
+  network {
+    uuid = openstack_networking_network_v2.network.id
+  }
+  security_groups = ["default", "allow-all"]
+}
+resource "openstack_compute_volume_attach_v2" "attached" {
+  instance_id = openstack_compute_instance_v2.instance.id
+  volume_id   = openstack_blockstorage_volume_v3.data.id
+}
