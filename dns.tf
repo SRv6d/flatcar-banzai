cc3a3f2a462192952dcdb79be7173f301023cb75
Marvin Vogt <m@rvinvogt.com>
2024-08-25 18:52:40 +0000

    Add workaround for incorrectly formatted instance access IP

--- dns.tf
+++ dns.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "banzai-v6" {
   zone_id = var.cf_zone_id
   name    = "banzai"
-  content = openstack_compute_instance_v2.instance.access_ip_v6
+  content = trim(openstack_compute_instance_v2.instance.access_ip_v6, "[]") // Trim used as workaround for https://github.com/terraform-provider-openstack/terraform-provider-openstack/issues/1357
   type    = "AAAA"
   ttl     = 300
 }
