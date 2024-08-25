cb63f00931c467a59479d5798e83de641ec9f925
Marvin Vogt <m@rvinvogt.com>
2024-08-25 17:13:13 +0000

    Use floating IP for A record

--- dns.tf
+++ dns.tf
@@ -9,7 +9,7 @@ resource "cloudflare_record" "banzai-v6" {
 resource "cloudflare_record" "banzai-v4" {
   zone_id = var.cf_zone_id
   name    = "banzai"
-  content = openstack_compute_instance_v2.instance.access_ip_v4
+  content = openstack_networking_floatingip_v2.default.address
   type    = "A"
   ttl     = 300
 }
