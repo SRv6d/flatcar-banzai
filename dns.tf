01b0afa367a08687556754a3d6915d93343518d4
Marvin Vogt <m@rvinvogt.com>
2024-08-26 21:56:42 +0200

    Decrese DNS TTL to 60 seconds (#17)

--- dns.tf
+++ dns.tf
@@ -3,7 +3,7 @@ resource "cloudflare_record" "banzai-v6" {
   name    = "banzai"
   content = trim(openstack_compute_instance_v2.instance.access_ip_v6, "[]") // Trim used as workaround for https://github.com/terraform-provider-openstack/terraform-provider-openstack/issues/1357
   type    = "AAAA"
-  ttl     = 300
+  ttl     = 60
 }
 
 resource "cloudflare_record" "banzai-v4" {
@@ -11,5 +11,5 @@ resource "cloudflare_record" "banzai-v4" {
   name    = "banzai"
   content = openstack_networking_floatingip_v2.default.address
   type    = "A"
-  ttl     = 300
+  ttl     = 60
 }
